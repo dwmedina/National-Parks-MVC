b5bb8f74cb5eb0ffe901bca498a6a4f41293d1e4
Sarah Robertson <robertson_sarah@yahoo.com>
2018-07-18 16:34:49 -0400

    Fiddled with ParkDetailView CSS, Added Survey DAL

---
+++ Capstone.Web/DALs/ISurveySqlDAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Capstone.Web.Models;
+
+namespace Capstone.Web.DALs
+{
+    public interface ISurveySqlDAL
+    {
+		Dictionary<string, int> GetFavoriteParkBySurveys();
+
+		void SaveNewSurvey(Survey survey);
+    }
+}
